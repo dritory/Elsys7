731b380fde02f0633c9de1debc7f7af9279bf454
endre dåvøy <lolskap@live.no>
2017-10-18 12:57:24 +0200

    boot

--- pycom/boot.py
+++ pycom/boot.py
@@ -1,9 +1,11 @@
 
 from machine import UART
+from network import bluetooth
 import machine
 import os
 
 uart = UART(0, baudrate=115200)
 os.dupterm(uart)
-
+pycom.wifi_on_boot(False)
+bluetooth.deinit()
 machine.main('main.py')
