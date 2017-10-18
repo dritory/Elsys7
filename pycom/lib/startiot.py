45875cc6a5c9ce8d83ccd7c336000c007efbe317
endre dåvøy <lolskap@live.no>
2017-10-18 09:29:49 +0200

    Folder

---
+++ pycom/lib/startiot.py
@@ -0,0 +1,34 @@
+from network import LoRa
+import socket
+import time
+import binascii
+import pycom
+import machine
+
+class Startiot:
+
+    def __init__(self):
+        self.dev_eui = binascii.unhexlify("000000000000034d")
+        self.app_eui = binascii.unhexlify("00000000000000ee")
+        self.app_key = binascii.unhexlify("77504d7273735173567262527171324b")
+
+        self.lora = LoRa(mode=LoRa.LORAWAN)
+    def connect(self, blocking):
+        self.lora.join(activation=LoRa.OTAA, auth=(self.dev_eui, self.app_eui, self.app_key), timeout=0)
+
+        while not self.lora.has_joined():
+            pass
+
+        self.s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+        # set the LoRaWAN data rate
+        self.s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+
+        # make the socket non-blocking
+        self.s.setblocking(blocking)
+
+    def send(self, data):
+        self.s.send(data)
+
+    def recv(self, length):
+        return self.s.recv(length)
